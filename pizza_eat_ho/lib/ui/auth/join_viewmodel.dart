8a3d689443fd0fa14f2b8f46a413e77d4dac84cf
uleh298 <uleh298@yonsei.ac.kr>
2025-12-22 14:11:36 +0900

    로그인+회원가입

---
+++ pizza_eat_ho/lib/ui/auth/join_viewmodel.dart
@@ -0,0 +1,39 @@
+﻿import 'package:flutter/foundation.dart';
+import 'package:pizzaeatho/data/model/user.dart';
+import 'package:pizzaeatho/data/repository/user_repository.dart';
+
+class JoinViewModel with ChangeNotifier {
+  final UserRepository _userRepository;
+
+  bool get isLoading => _isLoading;
+  bool _isLoading = false;
+
+  String? get errorMessage => _errorMessage;
+  String? _errorMessage;
+
+  JoinViewModel({UserRepository? userRepository})
+      : _userRepository = userRepository ?? UserRepository();
+
+  Future<bool> signup({
+    required String id,
+    required String pw,
+    required String name,
+  }) async {
+    _isLoading = true;
+    _errorMessage = null;
+    notifyListeners();
+
+    try {
+      await _userRepository.signup(
+        UserSignupRequestDto(id: id, pw: pw, name: name),
+      );
+      return true;
+    } catch (e) {
+      _errorMessage = e.toString();
+      return false;
+    } finally {
+      _isLoading = false;
+      notifyListeners();
+    }
+  }
+}
